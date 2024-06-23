731200e75a1691d7d5af562a91a18672db78a7ea
anarchking <stephen.littman7912@gmail.com>
2024-06-23 13:49:42 -0400

    Added a few pytests.

--- test_project.py
+++ test_project.py
@@ -23,6 +23,7 @@ def test_server_to_db():
     assert server_to_db("192.168.0.1", "65537") == False
 
 
+# make sure output matches input
 def test_load_last_server():
     server_to_db("192.168.1.1", "1883")
     assert load_last_server() == "Connect to Server 192.168.1.1 on Port 1883?"
@@ -32,6 +33,7 @@ def test_load_last_server():
     assert load_last_server() == "Connect to Server 192.168.4.1 on Port 1884?"
 
 
+# test for absurd values
 def test_connect_server():
     with pytest.raises(ValueError):
         connect_server("192.168.42.3", "string")
