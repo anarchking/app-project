e6de3adb96c22af7c9fa5a5cbabb3de7580f42cb
Stephen Littman <Stephen.littman7912@gmail.com>
2024-07-29 00:53:51 -0400

    Delete .buildozer directory

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webpdecoder_static/src/dsp/rescaler_msa.o.d
+++
@@ -1,8 +0,0 @@
-/home/anarchking/CS50P/project/project/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webpdecoder_static/src/dsp/rescaler_msa.o: \
-  /home/anarchking/CS50P/project/project/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/rescaler_msa.c \
-  /home/anarchking/CS50P/project/project/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h \
-  /home/anarchking/CS50P/project/project/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h
-
-/home/anarchking/CS50P/project/project/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/dsp/dsp.h:
-
-/home/anarchking/CS50P/project/project/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/types.h:
